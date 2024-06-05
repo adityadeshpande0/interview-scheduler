790e85f37929618cf4adc41cb1611b71a15afe18
adityadeshpande0 <adityadeshpande1@outlook.com>
2024-06-05 17:30:03 +0530

    Chnages Made

--- src/components/Scheduler.jsx
+++ src/components/Scheduler.jsx
@@ -10,61 +10,61 @@ function Scheduler() {
 
   return (
     <div className="p-4">
-    <div className="container-fluid card d-flex justify-content-center align-items-center bg-light col-lg-5 col-md-8 mt-4 p-4">
-      <div className="col-12 col-md-12 col-lg-12">
-        <h2 className="text-center mb-4">Schedule Your Interview</h2>
-        <div>
-          <InputField label="Date of Interview" type="date" />
-          <div className="mt-3">
-            <label>Select Preferred Time Slot</label>
-            <select className="form-select" name="timeSlot" id="timeSlot">
-              <option value="11 AM to 12 PM">11 AM to 12 PM</option>
-              <option value="1 PM to 2 PM">1 PM to 2 PM</option>
-              <option value="3 PM to 4 PM">3 PM to 4 PM</option>
-              <option value="5 PM to 6 PM">5 PM to 6 PM</option>
-            </select>
-          </div>
-          <div className="row mt-3 mb-3">
-            <span>Payment Status</span>
-            <div className="col-6 d-flex align-items-center mt-2">
-              <input
-                id="paid"
-                className="form-check-input me-1"
-                name="paymentStatus"
-                value={true}
-                type="radio"
-                checked={paymentStatus}
-                onChange={isPaid}
-              />
-              <label className="form-check-label" htmlFor="paid">
-                Paid
-              </label>
+      <div className="container-fluid card d-flex justify-content-center align-items-center bg-light col-lg-5 col-md-8 mt-4 p-4">
+        <div className="col-12 col-md-12 col-lg-12">
+          <h2 className="text-center mb-4">Schedule Your Interview</h2>
+          <div>
+            <InputField label="Date of Interview" type="date" />
+            <div className="mt-3">
+              <label>Select Preferred Time Slot</label>
+              <select className="form-select" name="timeSlot" id="timeSlot">
+                <option value="11 AM to 12 PM">11 AM to 12 PM</option>
+                <option value="1 PM to 2 PM">1 PM to 2 PM</option>
+                <option value="3 PM to 4 PM">3 PM to 4 PM</option>
+                <option value="5 PM to 6 PM">5 PM to 6 PM</option>
+              </select>
             </div>
-            <div className="col-6 d-flex align-items-center">
-              <input
-                id="unpaid"
-                className="form-check-input me-1"
-                name="paymentStatus"
-                value={false}
-                type="radio"
-                checked={!paymentStatus}
-                onChange={isPaid}
-              />
-              <label className="form-check-label" htmlFor="unpaid">
-                Pending
-              </label>
+            <div className="row mt-3 mb-3">
+              <span>Payment Status</span>
+              <div className="col-6 d-flex align-items-center mt-2">
+                <input
+                  id="paid"
+                  className="form-check-input me-1"
+                  name="paymentStatus"
+                  value={true}
+                  type="radio"
+                  checked={paymentStatus}
+                  onChange={isPaid}
+                />
+                <label className="form-check-label" htmlFor="paid">
+                  Paid
+                </label>
+              </div>
+              <div className="col-6 d-flex align-items-center">
+                <input
+                  id="unpaid"
+                  className="form-check-input me-1"
+                  name="paymentStatus"
+                  value={false}
+                  type="radio"
+                  // checked={!paymentStatus}
+                  onChange={isPaid}
+                />
+                <label className="form-check-label" htmlFor="unpaid">
+                  Pending
+                </label>
+              </div>
             </div>
+            {paymentStatus && (
+              <InputField
+                label="Transaction Id"
+                placeholder="Paste your transaction ID here"
+              />
+            )}
+            <button className="btn btn-primary mt-3 w-100">Send Request</button>
           </div>
-          {paymentStatus && (
-            <InputField
-              label="Transaction Id"
-              placeholder="Paste your transaction ID here"
-            />
-          )}
-          <button className="btn btn-primary mt-3 w-100">Send Request</button>
         </div>
       </div>
-    </div>
     </div>
   );
 }
