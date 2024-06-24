1bc33ac31dedbbd124e57097aa5297d467e9dc5a
adityadeshpande0 <adityadeshpande1@outlook.com>
2024-06-25 02:52:00 +0530

    API Integration Improved

--- src/components/User/UserRequests.jsx
+++ src/components/User/UserRequests.jsx
@@ -21,7 +21,7 @@ function UserRequests() {
         },
       });
       setRequests(response.data.interviewRequests || []); // Ensure it's an array
-      setIsAdmin(response.data.isAdmin === 'admin');
+      setIsAdmin(response.data.isAdmin === "admin");
     } catch (error) {
       console.error("Error fetching interview requests:", error);
       setError("Error fetching interview requests!");
@@ -39,7 +39,9 @@ function UserRequests() {
       return;
     }
 
-    const APIURL = `${import.meta.env.VITE_API_URL}/interview-requests/${requestId}/withdraw`;
+    const APIURL = `${
+      import.meta.env.VITE_API_URL
+    }/interview-requests/${requestId}/withdraw`;
     try {
       await axios.delete(APIURL, {
         headers: {
@@ -60,13 +62,19 @@ function UserRequests() {
       return;
     }
 
-    const APIURL = `${import.meta.env.VITE_API_URL}/interview-requests/${requestId}/approve`;
+    const APIURL = `${
+      import.meta.env.VITE_API_URL
+    }/interview-requests/${requestId}/approve`;
     try {
-      await axios.post(APIURL, {}, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      await axios.post(
+        APIURL,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
       fetchRequests();
     } catch (error) {
       console.error("Error approving interview request:", error);
@@ -85,6 +93,16 @@ function UserRequests() {
         {requests.length > 0 ? (
           requests.map((request) => (
             <div className="card col-12 col-lg-2 p-2 m-2" key={request._id}>
+              {request.userId && (
+                <div>
+                  <p>
+                    Name: <span>{request.userId.name}</span>
+                  </p>
+                  <p>
+                    Email: <span>{request.userId.email}</span>
+                  </p>
+                </div>
+              )}
               <p>
                 Date: <span>{new Date(request.date).toLocaleDateString()}</span>
               </p>
@@ -92,15 +110,32 @@ function UserRequests() {
                 Time Slot: <span>{request.timeslot}</span>
               </p>
               <p>
-                Status: <span>{request.status ? "Approved" : "Pending Approval"}</span>
+                Status:{" "}
+                <span>{request.status ? "Approved" : "Pending Approval"}</span>
               </p>
               {!isAdmin && ( // Hide the Withdraw button if isAdmin is true
-                <button className="btn btn-warning col-12" onClick={() => handleWithdrawRequest(request._id)}>Withdraw Request</button>
+                <button
+                  className="btn btn-warning col-12"
+                  onClick={() => handleWithdrawRequest(request._id)}
+                >
+                  Withdraw Request
+                </button>
               )}
               {isAdmin && (
                 <>
-                  <button className="btn btn-success col-12 mt-2" onClick={() => handleApproveRequest(request._id)} disabled={request.status}>Approve Request</button>
-                  <button className="btn btn-danger col-12 mt-2" onClick={() => handleWithdrawRequest(request._id)}>Reject Request</button>
+                  <button
+                    className="btn btn-success col-12 mt-2"
+                    onClick={() => handleApproveRequest(request._id)}
+                    disabled={request.status}
+                  >
+                    Approve Request
+                  </button>
+                  <button
+                    className="btn btn-danger col-12 mt-2"
+                    onClick={() => handleWithdrawRequest(request._id)}
+                  >
+                    Reject Request
+                  </button>
                 </>
               )}
             </div>
